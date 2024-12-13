0547f7af7bcf585ee3874915c31cbc559bc21eaf
Krozlov <lkencana06@gmail.com>
2024-12-13 20:31:20 +0700

    Update player.py

--- code/player.py
+++ code/player.py
@@ -8,7 +8,7 @@ from skin import *
 class Player(Entity):
 	def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack,create_magic):
 		super().__init__(groups)
-		self.image = pygame.image.load(f'{init_path}graphics/test/player.png').convert_alpha()
+		self.image = pygame.image.load(f'{init_path}graphics/player/Timo/down_idle/idle_down.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(-6,HITBOX_OFFSET['player'])
 
